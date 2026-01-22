75e065aceb7b4c55f31a13c8d1c674c35a705517
UsrNameJohn <141208422+UsrNameJohn@users.noreply.github.com>
2026-01-22 12:30:17 +0100

    Add barcode server

--- server.js
+++ server.js
@@ -1,1 +1,43 @@
+const http = require('http');
+const url = require('url');
+const bwipjs = require('bwip-js');
+
+const PORT = process.env.PORT || 10000;
+
+const server = http.createServer((req, res) => {
+    const query = url.parse(req.url, true).query;
+
+    if (!query.text) {
+        res.writeHead(400, { 'Content-Type': 'text/plain' });
+        res.end('Missing ?text parameter');
+        return;
+    }
+
+    try {
+        const png = bwipjs.toBuffer({
+            bcid: 'gs1databarexpandedstacked',
+            text: query.text,
+            scaleX: 2,
+            scaleY: 1,
+            includetext: true,
+            alttext: query.text,
+        });
+
+        png.then(buffer => {
+            res.writeHead(200, { 'Content-Type': 'image/png' });
+            res.end(buffer);
+        }).catch(err => {
+            res.writeHead(500, { 'Content-Type': 'text/plain' });
+            res.end(err.toString());
+        });
+
+    } catch (e) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end(e.toString());
+    }
+});
+
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
 
